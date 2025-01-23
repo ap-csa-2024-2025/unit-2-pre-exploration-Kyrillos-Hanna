4b0703290ab0faf6a611653515acb78aa1133868
Kyrillos-Hanna <kyrohanna90@gmail.com>
2025-01-23 16:59:57 +0000

    finished the warmup activity

--- WarmUp.java
+++ WarmUp.java
@@ -2,10 +2,33 @@ public class WarmUp
 {
   public static void main(String[] args)
   {
-    // test your methods here
+    int a = 2;
+    int b = 12;
+    int c = 5;
+    int d = 20;
+    int e = 7;
+    int f = 35;
+
+    System.out.println(isEven(a));
+    System.out.println(isEven(b));
+    System.out.println(isEven(c));
+    System.out.println(isEven(d));
+    System.out.println(isEven(e));
+    System.out.println(isEven(f));
+
+    System.out.println(isDivisible(b, a));
+    System.out.println(isDivisible(f, d));
+    System.out.println(isDivisible(d, c));
+    System.out.println(isDivisible(a, f));
   }
 
   // write your methods below
+  public boolean isEven(int num) {
+    return (num % 2) == 0;
+  }
 
+  public isDivisible(int a, int b) {
+    return (a % b) == 0;
+  }
   
 }
